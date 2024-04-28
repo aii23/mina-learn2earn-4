2ee7d25681b73e97b3ad87ddecd2a2884e1b582f
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-04-29 00:23:03 +0300

    Make messages secure + added additional info

--- packages/chain/src/SpyManager.ts
+++ packages/chain/src/SpyManager.ts
@@ -1,94 +1,164 @@
 import {
-    RuntimeModule,
-    runtimeMethod,
-    runtimeModule,
-    state,
+  RuntimeModule,
+  runtimeMethod,
+  runtimeModule,
+  state,
 } from '@proto-kit/module';
-import { StateMap, assert } from '@proto-kit/protocol';
+import { State, StateMap, assert } from '@proto-kit/protocol';
 import {
-    Bool,
-    Character,
-    CircuitString,
-    Field,
-    Poseidon,
-    Provable,
-    Struct,
+  Bool,
+  Character,
+  CircuitString,
+  Experimental,
+  Field,
+  Poseidon,
+  Provable,
+  PublicKey,
+  Struct,
+  UInt64,
 } from 'o1js';
 
 function getRandomInt(max: number): number {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 const stringValues = '01234567890abcdefghijklmnopqrstuvwxyz';
 
 export class SpyMessage extends Struct({
-    agentId: Field,
-    value: Provable.Array(Character, 12),
-    securityCode: Provable.Array(Character, 2),
+  agentId: Field,
+  value: Provable.Array(Character, 12),
+  securityCode: Provable.Array(Character, 2),
 }) {
-    getSecurityCodeHash(): Field {
-        return Poseidon.hash(this.securityCode.map((char) => char.toField()));
-    }
+  getSecurityCodeHash(): Field {
+    return Poseidon.hash(this.securityCode.map((char) => char.toField()));
+  }
 
-    static random(agentId: Field, securityCode: Character[]): SpyMessage {
-        return new SpyMessage({
-            agentId,
-            value: [...Array(12)].map((index) => {
-                let val = stringValues[getRandomInt(stringValues.length)];
-                return Character.fromString(
-                    stringValues[getRandomInt(stringValues.length)]
-                );
-            }),
-            securityCode: securityCode,
-        });
-    }
+  static random(agentId: Field, securityCode: Character[]): SpyMessage {
+    return new SpyMessage({
+      agentId,
+      value: [...Array(12)].map((index) => {
+        let val = stringValues[getRandomInt(stringValues.length)];
+        return Character.fromString(
+          stringValues[getRandomInt(stringValues.length)]
+        );
+      }),
+      securityCode: securityCode,
+    });
+  }
 }
 
 export class SpyStatus extends Struct({
-    active: Bool,
-    lastMessageId: Field,
-    securityCodeHash: Field,
+  active: Bool,
+  lastMessageId: Field,
+  securityCodeHash: Field,
 }) {}
+
+export class AditionalSpyInfo extends Struct({
+  blockHeigh: UInt64,
+  sender: PublicKey,
+  nonce: UInt64,
+}) {}
+
+export class SpyMessageProofPublicInput extends Struct({
+  securityCodeHash: Field,
+}) {}
+
+export class SpyMessageProofPublicOputput extends Struct({}) {}
+
+export const proveMessage = (
+  publicInput: SpyMessageProofPublicInput,
+  message: SpyMessage
+): SpyMessageProofPublicOputput => {
+  assert(publicInput.securityCodeHash.equals(message.getSecurityCodeHash()));
+  // // Value is 12 length by default
+
+  return new SpyMessageProofPublicOputput({});
+};
+
+export const MessageApp = Experimental.ZkProgram({
+  publicInput: SpyMessageProofPublicInput,
+  publicOutput: SpyMessageProofPublicOputput,
+  methods: {
+    proveMessage: {
+      privateInputs: [SpyMessage],
+      method: proveMessage,
+    },
+  },
+});
+
+export class MessageProof extends Experimental.ZkProgram.Proof(MessageApp) {}
 
 interface SpyManagerConfig {}
 
 @runtimeModule()
 export class SpyManager extends RuntimeModule<SpyManagerConfig> {
-    @state() public spyStatuses = StateMap.from<Field, SpyStatus>(
-        Field,
-        SpyStatus
+  @state() public spyStatuses = StateMap.from<Field, SpyStatus>(
+    Field,
+    SpyStatus
+  );
+
+  @runtimeMethod()
+  public addSpy(id: Field, securityCodeHash: Field) {
+    assert(
+      this.spyStatuses.get(id).value.active.not(),
+      'Spy with such id already exist'
     );
 
-    @runtimeMethod()
-    public addSpy(id: Field, securityCodeHash: Field) {
-        assert(
-            this.spyStatuses.get(id).value.active.not(),
-            'Spy with such id already exist'
-        );
+    this.spyStatuses.set(
+      id,
+      new SpyStatus({
+        active: Bool(true),
+        lastMessageId: Field.from(0),
+        securityCodeHash,
+      })
+    );
+  }
 
-        this.spyStatuses.set(
-            id,
-            new SpyStatus({
-                active: Bool(true),
-                lastMessageId: Field.from(0),
-                securityCodeHash,
-            })
-        );
-    }
+  @runtimeMethod()
+  public sendMessage(
+    agentId: Field,
+    messageId: Field,
+    messageProof: MessageProof
+  ) {
+    let spyStatus = this.spyStatuses.get(agentId).value;
 
-    @runtimeMethod()
-    public sendMessage(messageId: Field, message: SpyMessage) {
-        let spyStatus = this.spyStatuses.get(message.agentId).value;
+    messageProof.verify();
 
-        assert(spyStatus.active, 'This agent is not active');
-        assert(
-            spyStatus.securityCodeHash.equals(message.getSecurityCodeHash()),
-            'Wrong security code'
-        );
-        // Value is 12 length by default
-        assert(messageId.greaterThan(spyStatus.lastMessageId), 'Old message');
+    assert(
+      messageProof.publicInput.securityCodeHash.equals(
+        spyStatus.securityCodeHash
+      )
+    );
 
-        spyStatus.lastMessageId = messageId;
-        this.spyStatuses.set(message.agentId, spyStatus);
-    }
+    assert(spyStatus.active, 'This agent is not active');
+    assert(messageId.greaterThan(spyStatus.lastMessageId), 'Old message');
+
+    spyStatus.lastMessageId = messageId;
+    this.spyStatuses.set(agentId, spyStatus);
+  }
+}
+
+@runtimeModule()
+export class SpyManagerExtended extends SpyManager {
+  @state() public additionalInfo = StateMap.from<Field, AditionalSpyInfo>(
+    Field,
+    AditionalSpyInfo
+  );
+
+  @runtimeMethod()
+  public override sendMessage(
+    agentId: Field,
+    messageId: Field,
+    messageProof: MessageProof
+  ) {
+    super.sendMessage(agentId, messageId, messageProof);
+
+    let addInfo = this.additionalInfo.get(agentId).value;
+
+    addInfo.blockHeigh = this.network.block.height;
+    addInfo.nonce = addInfo.nonce.add(1);
+    addInfo.sender = this.transaction.sender.value;
+
+    this.additionalInfo.set(agentId, addInfo);
+  }
 }
--- packages/chain/test/SpyManager.test.ts
+++ packages/chain/test/SpyManager.test.ts
@@ -1,111 +1,159 @@
 import { TestingAppChain } from '@proto-kit/sdk';
 import { Character, Field, Poseidon, PrivateKey, UInt64 } from 'o1js';
-import { SpyManager, SpyMessage } from '../src/SpyManager';
 import { log } from '@proto-kit/common';
+import {
+  MessageProof,
+  SpyManager,
+  SpyManagerExtended,
+  SpyMessage,
+  SpyMessageProofPublicInput,
+  proveMessage,
+} from '../src/SpyManager';
 import { TransactionExecutionResult } from '@proto-kit/sequencer';
+import { Pickles } from 'o1js/dist/node/snarky';
+import { dummyBase64Proof } from 'o1js/dist/node/lib/proof_system';
 
 log.setLevel('ERROR');
 
+export async function mockProof<I, O, P>(
+  publicOutput: O,
+  ProofType: new ({
+    proof,
+    publicInput,
+    publicOutput,
+    maxProofsVerified,
+  }: {
+    proof: unknown;
+    publicInput: I;
+    publicOutput: any;
+    maxProofsVerified: 0 | 2 | 1;
+  }) => P,
+  publicInput: I
+): Promise<P> {
+  const [, proof] = Pickles.proofOfBase64(await dummyBase64Proof(), 2);
+  return new ProofType({
+    proof: proof,
+    maxProofsVerified: 2,
+    publicInput,
+    publicOutput,
+  });
+}
+
 describe('Spy Manager', () => {
-    it('Check that spy manager works fine', async () => {
-        const appChain = TestingAppChain.fromRuntime({
-            modules: {
-                SpyManager,
-            },
-        });
+  it('Check that spy manager works fine', async () => {
+    const appChain = TestingAppChain.fromRuntime({
+      SpyManagerExtended,
+    });
 
-        appChain.configurePartial({
-            Runtime: {
-                SpyManager: {},
-            },
-        });
+    appChain.configurePartial({
+      Runtime: {
+        SpyManagerExtended: {},
+        Balances: {},
+      },
+    });
 
-        await appChain.start();
-        const spyManager = appChain.runtime.resolve('SpyManager');
+    await appChain.start();
+    const spyManager = appChain.runtime.resolve('SpyManagerExtended');
 
-        const sendMessage = async (
-            senderSK: PrivateKey,
-            messageId: Field,
-            message: SpyMessage
-        ): Promise<TransactionExecutionResult> => {
-            const sender = senderSK.toPublicKey();
+    const sendMessage = async (
+      senderSK: PrivateKey,
+      messageId: Field,
+      message: SpyMessage
+    ): Promise<TransactionExecutionResult> => {
+      const sender = senderSK.toPublicKey();
 
-            appChain.setSigner(senderSK);
-            let tx = await appChain.transaction(sender, () => {
-                spyManager.sendMessage(messageId, message);
-            });
+      const publicInput = new SpyMessageProofPublicInput({
+        securityCodeHash: message.getSecurityCodeHash(),
+      });
 
-            await tx.sign();
-            await tx.send();
-            return (await appChain.produceBlock())!.transactions[0];
-        };
+      const publicOutput = proveMessage(publicInput, message);
+      const proof = await mockProof(publicOutput, MessageProof, publicInput);
 
-        const spy1Id = Field.from(1);
-        const spy1PrivateKey = PrivateKey.random();
-        const spy1 = spy1PrivateKey.toPublicKey();
-        const spy1SC = [Character.fromString('1'), Character.fromString('2')];
-        const spy1SCHash = Poseidon.hash(spy1SC.map((val) => val.toField()));
+      appChain.setSigner(senderSK);
+      let tx = await appChain.transaction(sender, () => {
+        spyManager.sendMessage(message.agentId, messageId, proof);
+      });
 
-        const spy2Id = Field.from(2);
-        const spy2PrivateKey = PrivateKey.random();
-        const spy2 = spy1PrivateKey.toPublicKey();
-        const spy2SC = [Character.fromString('a'), Character.fromString('b')];
-        const spy2SCHash = Poseidon.hash(spy2SC.map((val) => val.toField()));
+      await tx.sign();
+      await tx.send();
+      return (await appChain.produceBlock())!.transactions[0];
+    };
 
-        const noSpy2Id = Field.from(3);
-        const noSpyPrivateKey = PrivateKey.random();
-        const noSpy = spy1PrivateKey.toPublicKey();
-        const noSpySC = [Character.fromString('b'), Character.fromString('3')];
-        const noSpySCHash = Poseidon.hash(noSpySC.map((val) => val.toField()));
+    const spy1Id = Field.from(1);
+    const spy1PrivateKey = PrivateKey.random();
+    const spy1 = spy1PrivateKey.toPublicKey();
+    const spy1SC = [Character.fromString('1'), Character.fromString('2')];
+    const spy1SCHash = Poseidon.hash(spy1SC.map((val) => val.toField()));
 
-        // Register spy
-        appChain.setSigner(spy1PrivateKey);
-        let tx = await appChain.transaction(spy1, () => {
-            spyManager.addSpy(spy1Id, spy1SCHash);
-        });
+    const spy2Id = Field.from(2);
+    const spy2PrivateKey = PrivateKey.random();
+    const spy2 = spy1PrivateKey.toPublicKey();
+    const spy2SC = [Character.fromString('a'), Character.fromString('b')];
+    const spy2SCHash = Poseidon.hash(spy2SC.map((val) => val.toField()));
 
-        await tx.sign();
-        await tx.send();
-        await appChain.produceBlock();
+    const noSpy2Id = Field.from(3);
+    const noSpyPrivateKey = PrivateKey.random();
+    const noSpy = spy1PrivateKey.toPublicKey();
+    const noSpySC = [Character.fromString('b'), Character.fromString('3')];
+    const noSpySCHash = Poseidon.hash(noSpySC.map((val) => val.toField()));
 
-        tx = await appChain.transaction(spy1, () => {
-            spyManager.addSpy(spy2Id, spy2SCHash);
-        });
+    // Register spy
+    appChain.setSigner(spy1PrivateKey);
+    let tx = await appChain.transaction(spy1, () => {
+      spyManager.addSpy(spy1Id, spy1SCHash);
+    });
 
-        await tx.sign();
-        await tx.send();
-        await appChain.produceBlock();
+    await tx.sign();
+    await tx.send();
+    await appChain.produceBlock();
 
-        // Send right message
-        const right1 = SpyMessage.random(spy1Id, spy1SC);
-        let res = await sendMessage(spy1PrivateKey, Field(1), right1);
-        expect(res.status.toBoolean()).toBeTruthy();
+    tx = await appChain.transaction(spy1, () => {
+      spyManager.addSpy(spy2Id, spy2SCHash);
+    });
 
-        // Send wrong messageId
-        res = await sendMessage(spy1PrivateKey, Field(1), right1);
-        expect(res.status.toBoolean()).toBeFalsy();
+    await tx.sign();
+    await tx.send();
+    await appChain.produceBlock();
 
-        // Send wrong security code
-        const wrong1 = SpyMessage.random(spy1Id, spy2SC);
-        res = await sendMessage(spy1PrivateKey, Field(1), wrong1);
-        expect(res.status.toBoolean()).toBeFalsy();
+    // Send right message
+    const right1 = SpyMessage.random(spy1Id, spy1SC);
+    let res = await sendMessage(spy1PrivateKey, Field(1), right1);
+    expect(res.status.toBoolean()).toBeTruthy();
 
-        // Send noSpy message
-        const wrong2 = SpyMessage.random(noSpy2Id, noSpySC);
-        res = await sendMessage(noSpyPrivateKey, Field(1), wrong2);
-        expect(res.status.toBoolean()).toBeFalsy();
+    // Send wrong messageId
+    res = await sendMessage(spy1PrivateKey, Field(1), right1);
+    expect(res.status.toBoolean()).toBeFalsy();
 
-        // Send right transaction from spy2
-        const right2 = SpyMessage.random(spy2Id, spy2SC);
-        res = await sendMessage(spy2PrivateKey, Field(1), right2);
-        expect(res.status.toBoolean()).toBeTruthy();
+    // Send wrong security code
+    const wrong1 = SpyMessage.random(spy1Id, spy2SC);
+    res = await sendMessage(spy1PrivateKey, Field(1), wrong1);
+    expect(res.status.toBoolean()).toBeFalsy();
 
-        const spyStatus1 =
-            await appChain.query.runtime.SpyManager.spyStatuses.get(spy1Id);
-        const spyStatus2 =
-            await appChain.query.runtime.SpyManager.spyStatuses.get(spy2Id);
+    // Send noSpy message
+    const wrong2 = SpyMessage.random(noSpy2Id, noSpySC);
+    res = await sendMessage(noSpyPrivateKey, Field(1), wrong2);
+    expect(res.status.toBoolean()).toBeFalsy();
 
-        expect(spyStatus1?.lastMessageId.equals(Field.from(1))).toBeTruthy();
-        expect(spyStatus2?.lastMessageId.equals(Field.from(1))).toBeTruthy();
-    }, 1_000_000);
+    // Send right transaction from spy2
+    const right2 = SpyMessage.random(spy2Id, spy2SC);
+    res = await sendMessage(spy2PrivateKey, Field(1), right2);
+    expect(res.status.toBoolean()).toBeTruthy();
+
+    const spyStatus1 =
+      await appChain.query.runtime.SpyManagerExtended.spyStatuses.get(spy1Id);
+    const spyStatus2 =
+      await appChain.query.runtime.SpyManagerExtended.spyStatuses.get(spy2Id);
+
+    expect(spyStatus1?.lastMessageId.equals(Field.from(1))).toBeTruthy();
+    expect(spyStatus2?.lastMessageId.equals(Field.from(1))).toBeTruthy();
+
+    const spy1AdditionalInfo =
+      await appChain.query.runtime.SpyManagerExtended.additionalInfo.get(
+        spy1Id
+      );
+
+    console.log(spy1AdditionalInfo?.blockHeigh.toString());
+    console.log(spy1AdditionalInfo?.nonce.toString());
+    console.log(spy1AdditionalInfo?.sender.toString());
+  }, 1_000_000);
 });
